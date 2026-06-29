925299d6ecee71970faf3b36252aaae0bb13b65d
KaueSiqueira <kaue54150@gmail.com>
2026-06-29 17:55:37 -0300

    Aula 08

---
+++ Aulas/Aula 01/aula01.js
@@ -0,0 +1,4 @@
+"use strict"; //Modo restrito
+
+let nome = "kaue";
+console.log(nome);
---
+++ Aulas/Aula 08/aula08.js
@@ -0,0 +1,8 @@
+// Operações Bitwise - bit a bit
+
+let n1 = 20;
+let n2 = 14;
+
+let resDeslocamento = n1 << 1; // Para a esquerda, multiplica por 2, ao deslocar para a esquerda dobra
+let resDeslocamento2 = n1 >> 1; // Para a direita, divide por 2, ao deslocar para a direita, retorna a metade
+console.log(resDeslocamento);
