ca19979ac4bb12c0364bd2df47e775af1b2c40e6
Kaue <siqueiraprogrammer@gmail.com>
2026-09-21 20:05:11 -0300

    Template Strings

---
+++ Aulas/Aula 56 - Template Strings/index.js
@@ -0,0 +1,9 @@
+// Set - Não permite entrada de dados iguais, dados duplicados
+
+const caixa = document.querySelector("#caixa");
+
+const curso = "JavaScript";
+const nome = "Kaue";
+const frase = `Eu sou o ${nome} e estou me aprofundando em ${curso}`;
+
+caixa.innerHTML = frase;
